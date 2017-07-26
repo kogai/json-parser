3f95c9bd32e600c76fc7e25b36fe0af42078a938
shinichi kogai <kogai0121@gmail.com>
2017-07-26 14:07:40 +0900

    range

--- main.ml
+++ main.ml
@@ -1,1 +1,8 @@
-let () = Printf.printf "Hello world\n"
+let rec range n =
+  match n with
+  | 0 -> []
+  | _ -> n::(range (n -1))
+
+let () =
+  Printf.printf "Hello world\n";
+  List.iter print_int (range 10)
