0c68ab1de83aa224d32a56d4638cfeaa4bb43b4e
小飼慎一 <kogaishinichi@kogaishinichi-no-MacBook-Pro.local>
2017-07-30 15:08:39 +0900

    read number

--- src/lib/token.ml
+++ src/lib/token.ml
@@ -14,7 +14,7 @@ type tt =
 
 let is_digit s =
   try
-    ignore (float_of_string s);
+    ignore @@ float_of_string s;
     true
   with Failure e ->
     false
@@ -36,14 +36,45 @@ type line = int
 type column = int
 type token = tt * line * column
 
-let trim_head s =
+let head s =
+  String.sub s 0 1
+
+let tail s =
   String.sub s 1 @@ (s |> String.length |> pred)
 
-let rec tokenize = function
-  (*skip_white_space  *)
-    _ -> ((Some LBrace), "")
+let rec to_list = function
+  | "" -> []
+  | s -> (head s)::(to_list @@ tail s)
+
+(* let rec split_at = function *)
+
+let rec drop xs = function
+  | 0 -> xs
+  | n ->
+    match xs with
+    | [] -> []
+    | y::ys -> drop ys (n - 1)
+
+let read_number xs = 
+  let rec rn = function
+    | [] -> []
+    | x::xs when not @@ is_digit x -> []
+    | x::xs -> x::rn xs in
+  let result = rn xs |> String.concat "" in
+  let rest = drop xs @@ String.length result in
+  (Some (from_char result), rest)
+
+let rec tokenize_impl = function
+  (*read_string  *)
+  | " "::xs | "\n"::xs -> (None, xs)
+  | x::xs when is_digit x || x == "-" -> read_number (x::xs)
+  | xs -> (None, xs) (* for avoiding warn *)
+
 (* | " " -> (None, trim_head s) *)
 (* | _ -> ((String "ok", 10, 14), "残りの文字列")    *)
+
+let tokenize s =
+  tokenize_impl @@ to_list s
 
 let rec parse = function
   | _ -> true
--- test/test_main.ml
+++ test/test_main.ml
@@ -4,9 +4,10 @@ open OUnit2
 let specs = [
   "it should detect digit" >:: (fun ctx -> assert_equal true (is_digit "100"));
   "it should detect string" >:: (fun ctox -> assert_equal false (is_digit "string"));
-  "it should trim head of string" >:: (fun ctox -> assert_equal "first" @@ trim_head " first");
-  "it should tokenize" >:: (fun ctox ->
-      assert_equal ((Some LBrace), "") @@ tokenize "{");
+  "it should trim head of string" >:: (fun ctox -> assert_equal "first" @@ tail " first");
+  "it should tokenize" >:: (fun ctox -> assert_equal ((Some LBrace), "}") @@ tokenize "{}");
+  "it should tokenize" >:: (fun ctox -> assert_equal ((Some (StringT "foo")), "") @@ tokenize "\"foo\"");
+  "it should tokenize" >:: (fun ctox -> assert_equal ((Some (NumberT 100.0)), "") @@ tokenize "100");
 ]
 
 (* Name the test cases and group them together *)
