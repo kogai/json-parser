f6f0cea11d9215b6cafaadc1031d5e91798d6074
shinichi kogai <kogai0121@gmail.com>
2017-08-01 15:18:49 +0900

    fix identifier reader

--- src/lib/ast.ml
+++ src/lib/ast.ml
@@ -9,36 +9,46 @@ type t =
 exception InvaidToken of Token.t option
 
 let add k v = function
+  (* TODO: snoc関数で結合する or Stack構造を使う  *)
   | ObjectT xs -> ObjectT ((k, v)::xs)
   | ArrayT xs -> ArrayT (v::xs)
   | _ -> NullT
 
-let rec parse_array s container =
-  NullT
+let rec parse_array source container =
+  let (v, rest) = Token.token source in
+  let value = parse_value rest v in
+  let value_container = add "" value container in
 
-let rec parse_object s container = 
-  let (k, s1) = Token.token s in
+  match Token.token rest with
+  | (Some Token.Comma, s) -> parse_array s value_container
+  | x -> value_container
+
+and parse_object source container = 
+  let (k, s1) = Token.token source in
 
   match k with
   | Some Token.StringT key ->
     let (_, s2) = Token.token s1 in (* drop colon *)
     let (v, s3) = Token.token s2 in
-    let value = match v with
-      | Some Token.BoolT value -> BoolT value
-      | Some Token.StringT value -> StringT value
-      | Some Token.NumberT value -> NumberT value 
-      | Some Token.NullT -> NullT
-      | Some Token.LBrace -> parse_object s3 (ObjectT [])
-      | Some Token.LBracket -> parse_array s3 (ArrayT [])
-      | x -> raise @@ InvaidToken x in
+    let value = parse_value s3 v in
     let key_value_pair = add key value container in
     (match Token.token s3 with
      | (Some Token.Comma, s4) -> parse_object s4 key_value_pair
      | x -> key_value_pair)
   | x -> raise @@ InvaidToken x 
 
-let rec parse s =
-  let (token, s1) = Token.token s in
+and parse_value source v = 
+  match v with
+  | Some Token.BoolT value -> BoolT value
+  | Some Token.StringT value -> StringT value
+  | Some Token.NumberT value -> NumberT value 
+  | Some Token.NullT -> NullT
+  | Some Token.LBrace -> parse_object source (ObjectT [])
+  | Some Token.LBracket -> parse_array source (ArrayT [])
+  | x -> raise @@ InvaidToken x
+
+let rec parse source =
+  let (token, rest) = Token.token source in
   let root = match token with
     | Some Token.LBrace -> ObjectT []
     | Some Token.LBracket -> ArrayT []
@@ -47,7 +57,7 @@ let rec parse s =
   in
 
   match token with 
-  | Some Token.LBrace -> parse_object s1 root
-  | Some Token.LBracket -> parse_array s1 root
+  | Some Token.LBrace -> parse_object rest root
+  | Some Token.LBracket -> parse_array rest root
   | Some Token.EOF -> NullT 
   | x -> raise @@ InvaidToken x 
--- src/lib/token.ml
+++ src/lib/token.ml
@@ -49,23 +49,24 @@ let rec to_list = function
   | "" -> []
   | s -> (head s)::(to_list @@ tail s)
 
-let read_identifier predicate xs =
+let read_identifier is_string predicate xs =
   let rec read = function
     | [] -> ""
     | x::xs when not @@ predicate x -> ""
     | x::xs -> x ^ read xs in
   let result = read xs in
-  let length_of_identifier = (String.length result) + 1 in
+  let length_of_identifier = (String.length result) + (if is_string then 1 else 0) in
   let rest = drop length_of_identifier xs in
+  ExtLib.print rest;
   (Some (from_str result), rest)
 
 let token s =
   let impl = function
     | [] -> (Some EOF, [])
     | " "::xs | "\n"::xs -> (None, xs)
-    | x::xs when is_digit x || x = "-" -> read_identifier is_digit (x::xs)
-    | x::xs when is_letter x -> read_identifier is_letter (x::xs)
-    | x::xs when x = "\"" -> read_identifier is_letter @@ xs
+    | x::xs when is_digit x || x = "-" -> read_identifier false is_digit (x::xs)
+    | x::xs when is_letter x -> read_identifier false is_letter (x::xs)
+    | x::xs when x = "\"" -> read_identifier true is_letter @@ xs
     | x::xs -> (Some (from_str x), xs) in
   let (tkn, rest) = impl @@ to_list s in
   (tkn, String.concat "" rest)
