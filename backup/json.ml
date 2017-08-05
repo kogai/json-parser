c64837409c670d42dae49436169c06656966c812
小飼慎一 <kogaishinichi@kogaishinichi-no-MacBook-Pro.local>
2017-08-05 17:05:04 +0900

    backup current implement

---
+++ backup/json.ml
@@ -0,0 +1,37 @@
+open Core
+
+type t = [
+  | `ObjectT of (string * t) list
+  | `ArrayT of t list
+  | `BoolT of bool
+  | `StringT of string
+  | `FloatT of float
+  | `IntT of int
+  | `NullT
+]
+
+let rec output_value outc = function
+  | `ObjectT obj  -> print_assoc outc obj
+  | `ArrayT l     -> print_list outc l
+  | `StringT s   -> printf "\"%s\"" s
+  | `IntT i      -> printf "%d" i
+  | `FloatT x    -> printf "%f" x
+  | `BoolT true  -> Out_channel.output_string outc "true"
+  | `BoolT false -> Out_channel.output_string outc "false"
+  | `NullT       -> Out_channel.output_string outc "null"
+
+and print_assoc outc obj =
+  Out_channel.output_string outc "{ ";
+  let sep = ref "" in
+  List.iter ~f:(fun (key, value) ->
+      printf "%s\"%s\": %a" !sep key output_value value;
+      sep := ",\n  ") obj;
+  Out_channel.output_string outc " }"
+
+and print_list outc arr =
+  Out_channel.output_string outc "[";
+  List.iteri ~f:(fun i v ->
+      if i > 0 then
+        Out_channel.output_string outc ", ";
+      output_value outc v) arr;
+  Out_channel.output_string outc "]"
---
+++ backup/parser.mly
@@ -0,0 +1,35 @@
+%token <int> INT
+%token <float> FLOAT
+%token <string> STRING
+%token TRUE
+%token FALSE
+%token NULL
+%token LBRACE
+%token RBRACE
+%token LBRACKET
+%token RBRACKET
+%token COLON
+%token COMMA
+%token EOF
+
+%start <Json.t option> prog
+%%
+
+prog:
+  | EOF { None }
+  | v = value { Some v };
+value:
+  | LBRACE; v = object_fields; RBRACE { `ObjectT v }
+  | LBRACKET; v = array_values; RBRACKET { `ArrayT v }
+  | v = STRING { `StringT v }
+  | v = INT { `IntT v }
+  | v = FLOAT { `FloatT v }
+  | TRUE { `BoolT true }
+  | FALSE { `BoolT false }
+  | NULL { `NullT };
+object_fields:
+  v = separated_list(COMMA, object_field) { v };
+object_field:
+  k = STRING; COLON; v = value { (k, v) };
+array_values:
+  v = separated_list(COMMA, value) { v };
